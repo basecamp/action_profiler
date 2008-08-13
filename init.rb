7e2bd272002f532538e26a61dcdc19f2eb61d09d
Jeremy Kemper <jeremy@bitsweat.net>
2008-08-13 13:04:09 -0700

    Make Base.action_profiler a no-op with log message if ruby-prof isn't available.

--- init.rb
+++ init.rb
@@ -3,5 +3,11 @@ begin
   ActionController::Base.send :include, ActionController::ActionProfiler
   ActionController::Base.logger.info "Action profiling enabled. Add around_filter :action_profiler to ApplicationController then append ?profile=true to any URL to embed a call graph."
 rescue LoadError
-  ActionController::Base.logger.info "`gem install ruby-prof` to enable action profiling."
+  class ActionController::Base
+    def self.action_profiler(*args)
+      logger.info "`gem install ruby-prof` to enable action profiling."
+    end
+
+    action_profiler
+  end
 end
