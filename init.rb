591b75b927ae414dccd760be5c49e18abd00e321
Joshua Peek <josh@joshpeek.com>
2008-12-02 10:39:05 -0600

    don't explode if ruby-prof is not installed

--- init.rb
+++ init.rb
@@ -4,10 +4,9 @@ begin
   ActionController::Base.logger.info "Action profiling enabled. Add around_filter :action_profiler to ApplicationController then append ?profile=true to any URL to embed a call graph."
 rescue LoadError
   class ActionController::Base
-    def self.action_profiler(*args)
+    def action_profiler(*args)
       logger.info "`gem install ruby-prof` to enable action profiling."
+      yield
     end
-
-    action_profiler
   end
 end
